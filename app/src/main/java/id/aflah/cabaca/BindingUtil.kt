78265e8d5d9102b30d97892e32368e0be7878079
aflahtaqiu <aflahts98@gmail.com>
2020-06-15 23:57:25 +0700

    add image load databinding

---
+++ app/src/main/java/id/aflah/cabaca/BindingUtil.kt
@@ -0,0 +1,17 @@
+package id.aflah.cabaca
+
+import android.widget.ImageView
+import androidx.databinding.BindingAdapter
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
+
+object BindingUtil {
+
+    @JvmStatic
+    @BindingAdapter("imageUrlCrop")
+    fun setImageUrlCrop(imageView: ImageView, url: String?) {
+        Glide.with(imageView.context).load("https://cabaca.id:8443/api/v2/files/" + url + "&api_key=32ded42cfffb77dee86a29f43d36a3641849d4b5904aade9a79e9aa6cd5b5948")
+            .error(R.drawable.no_image)
+            .into(imageView)
+    }
+}
